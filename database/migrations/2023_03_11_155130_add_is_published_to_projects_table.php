202866baec8fccc0634648be0141085c7f73e253
AlessandroLembo <alessandrolembo2@gmail.com>
2023-03-11 17:04:24 +0100

    create migration to add column at projects table

---
+++ database/migrations/2023_03_11_155130_add_is_published_to_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            // Definisco una nuona colonna 
+            $table->boolean('is_published')->default(0)->after('web_platform');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            // Butto giù la colonna
+            $table->dropColumn('is_published');
+        });
+    }
+};
